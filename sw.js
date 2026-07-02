8663bcf37b663974a2d6d5c0d990c2cb25c41f77
skyfliessolar <skyfliessolar@gmail.com>
2026-07-02 07:29:32 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Enables offline support & install as app
 // ══════════════════════════════════════════════
 
-var CACHE_NAME = 'skyflies-v6';
+var CACHE_NAME = 'skyflies-v8';
 var URLS_TO_CACHE = [
   '/skyflies/',
   '/skyflies/index.html',
